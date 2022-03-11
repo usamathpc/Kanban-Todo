08309760f7a8eef5bb624b8fb9757d8f7ab41a8e
Usamath <usamathpallikkachalil@gmail.com>
2022-03-12 03:47:53 +0530

    cleanup main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,34 +1,30 @@
 import { Grid } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
+import { TodoContext } from "../TodoContext";
 import { TodoListType } from "../types";
-import { AddTodoDialog } from "./AddTodoDialog";
+import { AddEditTodoDialog } from "./AddEditTodoDialog";
 import { TodoList } from "./TodoList";
 
 type Props = {};
 
 export const Main = (props: Props) => {
-  const [open, setOpen] = React.useState(false);
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const { todoCards, inProgressCards, doneCards, addEditTodoModalProps } =
+    useContext(TodoContext);
 
   return (
     <>
-      <Grid container height={"100%"} spacing={2}>
+      <Grid container maxHeight={"100%"} height={"100%"} spacing={2}>
         <Grid item xs={12} md={4}>
-          <TodoList type={TodoListType.TODO} />
+          <TodoList cards={todoCards} type={TodoListType.TODO} />
         </Grid>
         <Grid item xs={12} md={4}>
-          <TodoList type={TodoListType.IN_PROGRESS} />
+          <TodoList cards={inProgressCards} type={TodoListType.IN_PROGRESS} />
         </Grid>
         <Grid item xs={12} md={4}>
-          <TodoList type={TodoListType.DONE} />
+          <TodoList cards={doneCards} type={TodoListType.DONE} />
         </Grid>
       </Grid>
-      <AddTodoDialog isOpen={open} handleClose={handleClose} />
+      <AddEditTodoDialog {...addEditTodoModalProps} />
     </>
   );
 };
