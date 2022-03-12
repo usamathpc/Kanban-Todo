04841b8eb3ee72c5f696785758f1107fa5fad7de
Usamath <usamathpallikkachalil@gmail.com>
2022-03-12 18:08:51 +0530

    update appbar styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,11 @@
+import { Avatar, useTheme } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import React from "react";
+import { SiTodoist } from "react-icons/si";
 
 const drawerWidth = 240;
 
@@ -12,18 +14,37 @@ type Props = {
 };
 
 export const Layout = (props: Props) => {
+  const theme = useTheme();
   return (
     <Box sx={{ display: "flex", bgcolor: "#F0F0F0" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
-        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          bgcolor: theme.palette.background.default,
+          boxShadow: `rgba(100, 100, 111, 0.2) 0px 0px 29px 0px`,
+          height: "70px",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          pl: 3,
+          pr: 3,
+        }}
       >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            MGS TODO
+        <Box sx={{ maxWidth: "1280px" }} display={"flex"} alignItems="center">
+          <SiTodoist fontSize={32} style={{ color: "black" }} />
+          <Typography ml={1} color="black" variant="h6" noWrap component="div">
+            MGS KANBAN TODO
           </Typography>
-        </Toolbar>
+        </Box>
+        <Box display="flex" alignItems={"center"}>
+          {/* <Typography mr={2} color="black">
+            Usamath PC
+          </Typography> */}
+          <Avatar src="https://robohash.org/b9faeb1635b244687618d4f3acbb4803?set=set4&bgset=&size=400x400" />
+        </Box>
       </AppBar>
 
       <Box
@@ -31,7 +52,7 @@ export const Layout = (props: Props) => {
         sx={{
           flexGrow: 1,
           p: 3,
-          mt: 8,
+          mt: 9,
         }}
       >
         {props.children}
